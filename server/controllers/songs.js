c53527b6b9c6a9b3b5554f1a12d61232eff0a6cc
GCodeOn <gsoto@rescueagency.com>
2022-10-27 07:12:00 -0700

    frontend: post request on song annotation change

--- server/controllers/songs.js
+++ server/controllers/songs.js
@@ -1,7 +1,7 @@
 const songModel = require('../database/models/song')
 
 exports.all = async (req, res, next) => {
-    console.log('songs api req', req.body);
+
     const songs = await songModel.find({})
     try {
         console.log('all song records', songs);
