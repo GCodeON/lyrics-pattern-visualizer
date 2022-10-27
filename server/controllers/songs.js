a03fe6414fa45e174119d6e541c274333fbc5b74
GCodeOn <gsoto@rescueagency.com>
2022-10-27 06:58:03 -0700

    backend: songs CRUD

--- server/controllers/songs.js
+++ server/controllers/songs.js
@@ -26,3 +26,26 @@ exports.add = async (req, res, next) => {
         res.status(400).json(error); 
     }
 }
+
+exports.update = async (req, res, next) => {
+    console.log("song id", req)
+    let id = req.params.id ? req.params.id : '63594cda7fa0722368b4227e';
+
+    let updateSong = {
+        title: req.body.title ? req.body.title : 'postman',
+        artist: req.body.artist ?  req.body.artist : 'postman',
+        lyrics: req.body.lyrics ?  req.body.lyrics : 'postman'
+    }
+    console.log('updated song', updateSong);
+    try {
+        await songModel.findByIdAndUpdate(id, updateSong);
+        console.log('existing song found', song);
+        await songModel.save(updateSong);
+        console.log('song saved sucessfully', song);
+
+        res.status(200).json(song); 
+    } catch (error) {
+        console.log
+        res.status(500).send(error);
+    }
+}
--- server/routes/songs.js
+++ server/routes/songs.js
@@ -5,5 +5,6 @@ const { songs } = require('../controllers/index');
 
 router.get('/', songs.all);
 router.post('/', songs.add);
+router.post('/:id', songs.update);
  
 module.exports = router
