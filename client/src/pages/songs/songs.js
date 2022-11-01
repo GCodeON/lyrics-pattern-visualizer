62970fabf14daf72b527532b31b4ab75dbeefb3b
GCodeON <gcodeondev@gmail.com>
2022-10-31 17:20:32 -0700

    firebase: updates

--- client/src/pages/songs/songs.js
+++ client/src/pages/songs/songs.js
@@ -31,6 +31,7 @@ class Songs extends React.Component {
             trackName   : null,
             trackArtist : null,
             loading     : false,
+            edit        : true
         }
     }
 
@@ -46,56 +47,6 @@ class Songs extends React.Component {
         })
     }
 
-    getSongs = async () => {
-        const songCollection = collection(db, "songs");
-        const data = await getDocs(songCollection)
-        console.log('firebase get docs', data.docs);
-    }
-
-    setSong = async (spotifyID, song) => {
-        const songSet = await setDoc(doc(db, 'songs', spotifyID), song);
-
-        console.log('set custom', songSet);
-    }
-
-    updateTrack = async (song) => {
-        const updateSong = doc(db, 'songs', song.spotify);
-
-        const updatedSong = await updateDoc(updateSong, song);
-
-        console.log('updatedSong', updatedSong)
-
-    }
-
-    findorCreate = async (track, data, type) => {
-        console.log(data.spotify);
-
-        const findSong = doc(db, "songs", data.spotify);
-
-        const song = await getDoc(findSong)
-
-        console.log('firebase get', song.data());
-
-       const savedSong = song.data();
-
-
-        if(song.exists()) {
-            console.log('firebase: song found', song.data());
-
-            if(type === 'get') {
-                this.setState({
-                    lyrics : savedSong.lyrics
-                })
-            } else {
-                this.updateTrack(data)   
-            } 
-
-        } else {
-            console.log('firebase: song not found');
-            this.setSong(track.id, data);
-        }
-        
-    }
 
     setActive(track) {
         let activeTrack = track;
@@ -113,6 +64,84 @@ class Songs extends React.Component {
             trackArtist  : trackArtist,
             trackFeature : trackFeature,
         })
+    }
+
+    getSongs = async () => {
+        const songCollection = collection(db, "songs");
+        const data = await getDocs(songCollection)
+        console.log('firebase get docs', data.docs);
+    }
+
+    setSong = async (spotifyID, song) => {
+        const songSet = await setDoc(doc(db, 'songs', spotifyID), song)
+
+      if(songSet) {
+        console.log('songSet', songSet);
+      }
+    }
+
+    updateTrack = async (song) => {
+        const updateSong = doc(db, 'songs', song.spotify);
+
+        const update = await updateDoc(updateSong, song);
+
+        console.log('record updated on change', this.state.lyrics);
+
+        this.setState({
+            lyrics : this.state.lyrics
+        })
+        
+    }
+
+    findorCreate = async (track, data, type) => {
+        console.log(data.spotify);
+
+        const findSong = doc(db, "songs", data.spotify);
+
+        const song = await getDoc(findSong)
+
+        // console.log('firebase get', song.data());
+ 
+       const savedSong = song.data();
+
+
+        if(song.exists()) {
+            // console.log('firebase: song found', song.data());
+
+            if(type === 'change') {
+                data.lyrics = savedSong.lyrics;
+                console.log( )
+                this.updateTrack(data);
+                
+                this.setState({
+                    lyrics : savedSong.lyrics
+                })
+            } else {
+             
+                this.setState({
+                    lyrics : savedSong.lyrics
+                })
+            } 
+
+        } else {
+            console.log('firebase: song not found');
+            this.setSong(track.id, data);
+        }
+        
+    }
+
+    handleChange(content) {
+        console.log('editor changed', content)
+
+
+        let songData = {
+            title   : this.state.trackName,
+            artist  : this.state.trackArtist,
+            spotify : this.state.activeTrack.id,
+            lyrics  : content
+        }
+
+        this.findorCreate(this.state.activeTrack, songData, 'change');
     }
 
     getLyrics = (track) => {
@@ -169,38 +198,29 @@ class Songs extends React.Component {
         return list;
     }
 
-    handleChange(content) {
-        // console.log('editor changed', content)
-
-        let songData = {
-            title   : this.state.trackName,
-            artist  : this.state.trackArtist,
-            spotify : this.state.activeTrack.id,
-            lyrics  : content
-        }
-
-        this.findorCreate(this.state.activeTrack, songData, 'change');
-        
-       
-    }
-
     displayLyrics() {
         let show;
         if(this.state.lyrics) {
             show = <div className="track">
                         <div className="editor-wrapper">  
-                            {/* <div dangerouslySetInnerHTML={{ __html: this.state.lyrics.replace(/\n/g, '<br />') }} />  */}
                             <div className="info">
                                 <h1 className="title">{ this.state.trackName }</h1>
                                 <h3 className="artist">{ this.state.trackArtist }</h3>
                             </div>
-                            <SunEditor
-                                onChange={ this.handleChange }
-                                setContents={ this.state.lyrics.replace(/\n/g, '<br />')}
-                                setOptions={{
-                                    height: 200,
-                                    buttonList: [['hiliteColor', 'fontColor']] 
-                                }} />
+
+                            { !this.state.edit  && (
+                                <div className="lyrics" dangerouslySetInnerHTML={{ __html: this.state.lyrics.replace(/\n/g, '<br />') }} /> 
+                            )}
+
+                            { this.state.edit  && (
+                                <SunEditor
+                                    onChange={ this.handleChange }
+                                    setContents={ this.state.lyrics.replace(/\n/g, '<br />')}
+                                    setOptions={{
+                                        height: 200,
+                                        buttonList: [['hiliteColor', 'fontColor']] 
+                                    }} />
+                            )}
                         </div>
                         <div className="analysis-type">
                             <RhymeScheme />
