293bb5a943bf2697e58ef01fde9f1e7339d939b7
GCodeON <gcodeondev@gmail.com>
2022-11-30 08:51:26 -0800

    Spotify Player refactor into own component seperate than the dashboard layout

---
+++ client/src/components/ui/spotify-player/index.js
@@ -0,0 +1,83 @@
+import { Outlet, useNavigate, useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import SpotifyPlayer from 'react-spotify-web-playback';
+
+import './styles.scss';
+
+const SpotifyWebPlayer = (props) => {
+  const classes =`${props.className ? props.className : ''}`;
+  const [spotifyToken, setSpotifyToken] = useState('');
+  const [ playerState, setPlayerState] = useState(true);
+  const [ activeTrack, setActiveTrack] = useState({});
+  const [ songChanged, setSongChange] = useState(false);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    setSpotifyToken(localStorage.getItem('spotify_token'));
+  },[]);
+
+  useEffect(() => {
+    var currentPage = window.location;
+    console.log('page path', currentPage);
+
+    if(songChanged) {
+      window.localStorage.setItem('active', JSON.stringify(activeTrack));
+     if(currentPage.pathname == '/') {
+      navigate(0);
+      // window.location.reload();
+     }
+    }
+  },[songChanged]);
+
+  function compareTrack(old, updated) {
+    if(updated.name !== old.name) {
+      console.log('different');
+      setActiveTrack(updated);
+      setSongChange(true);
+    } else {
+      console.log('same');
+      setSongChange(false);
+    }
+  }
+
+  const spotifyCallback = (state) => {
+    if(state.type == "status_update" && state.status !== "ERROR" && state.status == "READY" ) {
+      console.log("ready");
+    } else if(state.type == "player_update") {
+      console.log('player');
+    } else if(state.type == "device_update") {
+      // console.log('device');
+    } else if(state.type == "track_update") {
+      console.log('track');
+      let previousTrack = JSON.parse(window.localStorage.getItem('active'));
+      compareTrack(previousTrack ? previousTrack : {}, state.track);
+    }
+  }
+
+  return (
+    <div className="spotify-web-player">
+      {spotifyToken && activeTrack && (
+          <SpotifyPlayer
+            name={'DECODED Web Player'}
+            callback={(state) => spotifyCallback(state)}
+            syncExternalDeviceInterval={5}
+            persistDeviceSelection={true}
+            syncExternalDevice={true}
+            token={localStorage.getItem('spotify_token')}
+            styles={{
+              activeColor       : '#fff',
+              bgColor           : '#000',
+              color             : '#fff',
+              loaderColor       : '#fff',
+              trackArtistColor  : '#ccc',
+              trackNameColor    : '#fff',
+              sliderHandleColor : '#fff'
+
+            }}
+          />
+      )}
+    </div>
+  )
+}
+
+export default SpotifyWebPlayer;
--- client/src/layouts/dashboard.js
+++ client/src/layouts/dashboard.js
@@ -1,59 +1,20 @@
 import { Outlet, useNavigate, useHistory } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import SpotifyPlayer from 'react-spotify-web-playback';
 
 import Sidebar from '../components/ui/sidebar/sidebar';
+import SpotifyWebPlayer from '../components/ui/spotify-player';
 import './dashboard.scss';
 
 const Dashboard = (props) => {
   const classes =`${props.className ? props.className : ''}`;
-  const [spotifyToken, setSpotifyToken] = useState('');
-  const [ playerState, setPlayerState] = useState(true);
-  const [ activeTrack, setActiveTrack] = useState({});
-  const [ songChanged, setSongChange] = useState(false);
+
   const navigate = useNavigate();
   
   useEffect(() => {
-    setSpotifyToken(localStorage.getItem('spotify_token'));
+   
   },[]);
 
-  useEffect(() => {
-    var currentPage = window.location;
-    console.log('page path', currentPage);
 
-    if(songChanged) {
-      window.localStorage.setItem('active', JSON.stringify(activeTrack));
-     if(currentPage.pathname == '/') {
-      navigate(0);
-      // window.location.reload();
-     }
-    }
-  },[songChanged]);
-
-  function compareTrack(old, updated) {
-    if(updated.name !== old.name) {
-      console.log('different');
-      setActiveTrack(updated);
-      setSongChange(true);
-    } else {
-      console.log('same');
-      setSongChange(false);
-    }
-  }
-
-  const spotifyCallback = (state) => {
-    if(state.type == "status_update" && state.status !== "ERROR" && state.status == "READY" ) {
-      console.log("ready");
-    } else if(state.type == "player_update") {
-      console.log('player');
-    } else if(state.type == "device_update") {
-      // console.log('device');
-    } else if(state.type == "track_update") {
-      console.log('track');
-      let previousTrack = JSON.parse(window.localStorage.getItem('active'));
-      compareTrack(previousTrack ? previousTrack : {}, state.track);
-    }
-  }
 
   return (
     <div className="root-route">
@@ -64,28 +25,7 @@ const Dashboard = (props) => {
         </div>
       </div>
       <div className="fixed-footer">
-        {spotifyToken && activeTrack && (
-          <div className="spotify-web-player">
-            <SpotifyPlayer
-              name={'DECODED Web Player'}
-              callback={(state) => spotifyCallback(state)}
-              syncExternalDeviceInterval={5}
-              persistDeviceSelection={true}
-              syncExternalDevice={true}
-              token={localStorage.getItem('spotify_token')}
-              styles={{
-                activeColor       : '#fff',
-                bgColor           : '#000',
-                color             : '#fff',
-                loaderColor       : '#fff',
-                trackArtistColor  : '#ccc',
-                trackNameColor    : '#fff',
-                sliderHandleColor : '#fff'
-
-              }}
-            />
-          </div>
-        )}
+        <SpotifyWebPlayer />
       </div>
     </div>
   )
